06571f336d5ba727de0cd2cc2b6ec0cf2dfa2ccd
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-03 06:20:46 -0500

    Headers CORS añadida

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var logger = require('morgan');
 var app = express();
 
 app.use(logger('dev'));
+app.use(function (req, res, next) {
+  res.set('Server', 'Ubuntu');
+  res.set('Access-Control-Allow-Origin', '*');
+  return next();
+});
 app.use(express.static(path.join(__dirname, '')));
 app.listen(9090);
 console.log('Listening on port 9090');
