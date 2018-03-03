88066a1023f9fe346fe7ae25ea2b413ee1ac7393
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-03 06:10:00 -0500

    Imprime en consola los archivos pedidos

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+var logger = require('morgan');
+var app = express();
 
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '')));
-
 app.listen(9090);
 console.log('Listening on port 9090');
